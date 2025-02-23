058dc59d5edf57cee1ca7bf062e897feab3a4b8f
Izzy Dior <shop218916@gmail.com>
2025-02-23 12:04:10 -0600

    Created a functionjs file for lesson 6 project 02/10/25

--- functions.js
+++ functions.js
@@ -48,16 +48,11 @@ functionName(argument);  //  functionName needs to have ()
 //
 //}
 //
-//
-//
 //console.log(subtract(10, 9));
-//
-//const multiply = (uno, dos) => uno * dos
-//
-//// If the functions has one expression you don't have to add {} or return. You can add expression after =>
-//
-//        // PARAMETER CAN BE NAME ANYTHING... IT DOESN'T MATTER!!
-//
-// console.log(multiply(8, 2));
-//
-//
+
+
+const multiply = (uno, dos) => uno * dos
+// If the functions has one expression you don't have to add {} or return. You can add expression after =>
+       // PARAMETER CAN BE NAME ANYTHING... IT DOESN'T MATTER!!
+
+cconsole.log(multiply(8, 2));
