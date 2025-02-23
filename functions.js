f9ac2ed94a156194a887670532c4134e0ec0b150
Izzy Dior <shop218916@gmail.com>
2025-02-23 09:22:10 -0600

    Js functions project

---
+++ functions.js
@@ -0,0 +1,63 @@
+const functionName =(parameters) => {
+    // CODE TO EXECUTE
+   return value
+}
+
+functionName(argument);  //  functionName needs to have ()
+
+// const realine = require('readline-sync')
+
+
+//const greet = () => {
+//
+//    return "Hello World"
+//
+//}
+//
+//
+//
+//
+//
+//console.log(greet()); // greet needs to have ()
+//
+//const greetUser = (name) => {
+//
+//    return `Hello ${name}`;
+//
+//}
+//
+//
+//
+//console.log(greetUser("Izzy"));
+//
+//INTEGERS
+//
+//const add = (a, b) =>  {
+//
+//    return a + b;
+//
+//}
+//
+//
+//
+//console.log(add(5, 3));
+//
+//const subtract = (c, d) => {
+//
+//    return c - d;
+//
+//}
+//
+//
+//
+//console.log(subtract(10, 9));
+//
+//const multiply = (uno, dos) => uno * dos
+//
+//// If the functions has one expression you don't have to add {} or return. You can add expression after =>
+//
+//        // PARAMETER CAN BE NAME ANYTHING... IT DOESN'T MATTER!!
+//
+// console.log(multiply(8, 2));
+//
+//
